9fb4e3c7883f88183072b5520cd626090a2ddcde
Adrián Claveri <adrian.claveri@uruit.com>
2017-12-21 10:20:34 -0300

    Added initial validation rules

--- Steamboat.Mobile/ViewModels/InitPasswordViewModel.cs
+++ Steamboat.Mobile/ViewModels/InitPasswordViewModel.cs
@@ -9,13 +9,13 @@ namespace Steamboat.Mobile.ViewModels
     {
         #region Properties
 
-        //private ValidatableObject<string> _password;
-        private string _password;
+        private ValidatableObject<string> _password;
+        private ValidatableObject<string> _confirm;
         private bool _buttonEnabled;
 
         public ICommand ToggleCommand { get; set; }
-        //public ValidatableObject<string> Password { set { SetPropertyValue(ref _password, value); } get { return _password; } }
-        public string Password { set { SetPropertyValue(ref _password, value); } get { return _password; } }
+        public ValidatableObject<string> Password { set { SetPropertyValue(ref _password, value); } get { return _password; } }
+        public ValidatableObject<string> Confirm { set { SetPropertyValue(ref _confirm, value); } get { return _confirm; } }
         public bool ButtonEnabled { set { SetPropertyValue(ref _buttonEnabled, value); } get { return _buttonEnabled; } }
 
         #endregion
@@ -24,11 +24,42 @@ namespace Steamboat.Mobile.ViewModels
         {
             ToggleCommand = new Command(() => this.Toggle());
             ButtonEnabled = false;
+
+            Password = new ValidatableObject<string>();
+            Confirm = new ValidatableObject<string>();
         }
 
         private void Toggle()
         {
             ButtonEnabled = !ButtonEnabled;
         }
+
+        #region Validations
+
+        private bool Validate()
+        {            
+            bool isValidPassword = ValidatePassword();
+            bool isValidConfirm = ValidateConfirm();
+
+            return isValidConfirm && isValidPassword;
+        }
+
+        private bool ValidatePassword()
+        {
+            return Password.Validate();
+        }
+
+        private bool ValidateConfirm()
+        {
+            return Confirm.Validate();
+        }
+
+        private void AddValidations()
+        {
+            _password.Validations.Add(new IsNotNullOrEmptyRule<string> { ValidationMessage = "Enter a password" });
+            _confirm.Validations.Add(new IsNotNullOrEmptyRule<string> { ValidationMessage = "Enter a password" });
+        }
+
+        #endregion
     }
 }
