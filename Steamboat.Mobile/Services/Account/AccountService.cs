01fa702a7ee10b042867327cb25512988a77d63e
Adrián Claveri <adrian.claveri@uruit.com>
2017-12-26 14:11:31 -0300

    Implemented service

--- Steamboat.Mobile/Services/Account/AccountService.cs
+++ Steamboat.Mobile/Services/Account/AccountService.cs
@@ -29,9 +29,11 @@ namespace Steamboat.Mobile.Services.Account
             return await _requestProvider.PostAsync<AccountLogout>(url, sessionID:sessionId);
         }
 
-        public Task<AccountLogin> AccountInitPassword(AccountInitPassword passwords)
+        public async Task<AccountLogin> AccountInitPassword(AccountInitPassword passwords)
         {
-            throw new NotImplementedException();
+            string url = string.Format(ApiUrlBase + "{0}", "changepassword");
+
+            return await _requestProvider.PostAsync<AccountLogin, AccountInitPassword>(url, passwords);
         }
     }
 }
