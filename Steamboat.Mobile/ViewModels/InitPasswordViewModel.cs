84600149be72127b0bc1d93d9356f6dea90a8281
CARRASCO\rpintos <rodrigo.pintos@uruit.com>
2017-12-28 16:38:57 -0300

    passing status as parameter in navigation

--- Steamboat.Mobile/ViewModels/InitPasswordViewModel.cs
+++ Steamboat.Mobile/ViewModels/InitPasswordViewModel.cs
@@ -61,7 +61,7 @@ namespace Steamboat.Mobile.ViewModels
 
                 var status = await _participantManager.GetStatus();
                 var viewModelType = DashboardStatusHelper.GetViewModelForStatus(status.Dashboard.NextStepContent);
-                await NavigationService.NavigateToAsync(viewModelType);
+                await NavigationService.NavigateToAsync(viewModelType, status);
 
             }
             catch (Exception e)
