c5bd7b404a8275235a671a7c01a9c7ec01e45596
adrianclaveri <adrian.claveri@uruit.com>
2018-01-12 11:28:17 -0300

    Fix/scroll view on iOS (#10)
    * Initial fix
    
    * Added fix for previous versions of iOS

---
+++ iOS/CustomRenderers/CustomScrollViewRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Steamboat.Mobile.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ScrollView), typeof(CustomScrollViewRenderer))]
+namespace Steamboat.Mobile.iOS.CustomRenderers
+{
+    public class CustomScrollViewRenderer : ScrollViewRenderer
+    {
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            if(UIDevice.CurrentDevice.CheckSystemVersion(11,0))
+            {
+                this.ContentInsetAdjustmentBehavior = UIKit.UIScrollViewContentInsetAdjustmentBehavior.Never;   
+            }
+        }
+    }
+}
