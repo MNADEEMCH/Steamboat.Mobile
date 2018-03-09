398b7fe360a8a173b85fb343525f190cc66e272f
Rodrigo Pintos <rodrigo.pintos@uruit.com>
2018-03-09 17:46:01 -0300

    push para ios

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using FFImageLoading.Forms.Touch;
 using FFImageLoading.Svg.Forms;
+using Firebase.CloudMessaging;
 using Foundation;
 using Steamboat.Mobile.CustomControls;
 using UIKit;
+using UserNotifications;
 using UXDivers.Gorilla;
 
 namespace Steamboat.Mobile.iOS
 {
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate, Firebase.CloudMessaging.IMessagingDelegate
     {
+
+        public void DidRefreshRegistrationToken(Messaging messaging, string fcmToken)
+        {
+            System.Diagnostics.Debug.WriteLine($"FCM Token: {fcmToken}");
+        }
+
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
 
+            RegisterForPushNotifications();
+
             ResolveDependencies();
             CachedImageRenderer.Init();
             var ignore = typeof(SvgCachedImage);
+
             LoadApplication(new App());
             //LoadApplication(UXDivers.Gorilla.iOS.Player.CreateApplication(
             //  new UXDivers.Gorilla.Config("Good Gorilla")
@@ -29,6 +41,61 @@ namespace Steamboat.Mobile.iOS
             //));
 
             return base.FinishedLaunching(app, options);
+        }
+
+        public void RegisterForPushNotifications(){
+            
+            Firebase.Core.App.Configure();
+
+            // Register your app for remote notifications.
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) => {
+                    Console.WriteLine(granted);
+                });
+            }
+            else
+            {
+                // iOS 9 or before
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+        }
+
+        public override void RegisteredForRemoteNotifications(
+                                UIApplication application, NSData deviceToken)
+        {
+            // Get current device token
+            var DeviceToken = deviceToken.Description;
+            if (!string.IsNullOrWhiteSpace(DeviceToken))
+            {
+                DeviceToken = DeviceToken.Trim('<').Trim('>');
+            }
+
+            // Get previous device token
+            var oldDeviceToken = NSUserDefaults.StandardUserDefaults.StringForKey("PushDeviceToken");
+
+            // Has the token changed?
+            if (string.IsNullOrEmpty(oldDeviceToken) || !oldDeviceToken.Equals(DeviceToken))
+            {
+                //TODO: Put your own logic here to notify your server that the device token has changed/been created!
+            }
+
+            // Save new device token
+            NSUserDefaults.StandardUserDefaults.SetString(DeviceToken, "PushDeviceToken");
+        }
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+        {
+            //new UIAlertView("Error registering push notifications", error.LocalizedDescription, null, "OK", null).Show();
         }
 
         private void ResolveDependencies()
