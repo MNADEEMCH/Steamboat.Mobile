2af9e79f586b4987556000eba57aad47ddcbc552
adrianclaveri <adrian.claveri@uruit.com>
2018-03-13 16:22:31 -0300

    Used Xamanimation for FadeIn animation (#32)

--- Steamboat.Mobile/Views/PreScreeningInterviewView.xaml.cs
+++ Steamboat.Mobile/Views/PreScreeningInterviewView.xaml.cs
@@ -15,46 +15,16 @@ namespace Steamboat.Mobile.Views
 
         async Task Handle_ItemCreated(object sender, CustomControls.RepeaterControlItemAddedEventArgs args)
         {
-            
-            /*
-             * Native animations without Xamanimation Library
-             * args.View.Opacity = 0;
-            await Task.Delay(1);
-            await args.View.LayoutTo(new Rectangle(500, args.View.Y, args.View.Width, args.View.Height), 0);
-
-            var translateAnimation = args.View.LayoutTo(new Rectangle(0, args.View.Y, args.View.Width, args.View.Height), 500, Easing.CubicInOut);
-            var fadeInAnimation = args.View.FadeTo(1, 500, Easing.Linear);
-            await Task.WhenAll(fadeInAnimation);
-            */
-
-            args.View.Opacity = 0;
-            await Task.Delay(1);
             var view = args.View;
+            view.Opacity = 0;
+            await Task.Delay(1);
 
-            await view.Animate(new Xamanimation.TranslateToAnimation()
+            await view.Animate(new Xamanimation.FadeToAnimation()
             {
-                Duration = "1",
-                Easing = EasingType.Linear,
-                TranslateX = -500,
-                TranslateY = 0
-            });
-
-            var translate =  view.Animate(new Xamanimation.TranslateToAnimation()
-            {
-                Duration = "500",
-                Easing = EasingType.Linear,
-                TranslateX = 0,
-                TranslateY = 0
-            });
-
-            var fadeIn = view.Animate(new Xamanimation.FadeToAnimation()
-            {
-                Duration="800",
-                Easing = EasingType.Linear,
+                Duration = "800",
+                Easing = EasingType.CubicOut,
                 Opacity = 1
             });
-
-            await Task.WhenAll(fadeIn, translate);
         }
     }
 }
